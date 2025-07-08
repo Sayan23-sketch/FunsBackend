2a0e2179120f3ae922f76cbf65966331f65ac965
Sayan Majhi <161747341+Sayan23-sketch@users.noreply.github.com>
2025-07-08 15:46:47 +0000

    okk2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 
+// Register new user
 router.post("/register", register);
+
+// Login existing user
 router.post("/login", login);
 
 module.exports = router;
--- routes/fundRoutes.js
+++ routes/fundRoutes.js
@@ -6,14 +6,22 @@ const {
   getSavedFunds,
   addCustomFund,
   getCustomFunds,
-  removeSavedFund,
+  removeSavedFund
 } = require("../controllers/fundController");
 
-// Routes
+// Save a mutual fund (public or custom)
 router.post("/save", authMiddleware, saveFund);
+
+// Get all saved fund IDs for the logged-in user
 router.get("/saved", authMiddleware, getSavedFunds);
+
+// Add a custom fund (created manually by the user)
 router.post("/custom", authMiddleware, addCustomFund);
+
+// Get all custom funds created by the user
 router.get("/custom", authMiddleware, getCustomFunds);
+
+// Remove a saved fund by its fundId or _id
 router.delete("/remove/:id", authMiddleware, removeSavedFund);
 
 module.exports = router;
