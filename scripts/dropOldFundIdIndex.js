3dc9208b24a5b9f1c1013894dfdec8a634a6a46f
Sayan Majhi <161747341+Sayan23-sketch@users.noreply.github.com>
2025-07-09 05:39:50 +0000

    please

--- scripts/dropOldFundIdIndex.js
+++ scripts/dropOldFundIdIndex.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 
 const run = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ Connected to MongoDB");
 
     const collection = mongoose.connection.collection("savedfunds");
